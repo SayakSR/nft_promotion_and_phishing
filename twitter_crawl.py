8b86c1ee006214339f17d395f3225af06126e007
SayakSR <sayaksr05@gmail.com>
2022-05-24 16:36:00 -0400

    init twitter

---
+++ twitter_crawl.py
@@ -0,0 +1,34 @@
+# Third party imports
+
+import os
+import pandas as pd
+
+# Own imports
+
+from drivers.screenshot import take_screenshot
+
+# ==== Begin ==== 
+
+nft_keywords=['#NFT', '#NFTs', '#NFTArt', '#NFTGiveaway', '#NFTGiveaways', '#NFTdrop', '#NFTCommunity', '#NFTCollection']
+
+
+
+
+
+for i in nft_keywords:
+    filename=i.replace("#", "")
+    cmd_fetch="twarc2 search --limit 200"+ " '"+str(i)+"' output/"+str(filename)+".json" # Invokes twarc to fetch tweets using the academic API
+    cmd_convert_to_csv="twarc2 csv "+"output/"+str(filename)+".json"+" output/"+str(filename)+".csv" # Parses twarc json data to csv format 
+    os.system(cmd_fetch)
+    os.system(cmd_convert_to_csv)
+    #print(cmd_convert_to_csv)
+
+    
+
+
+
+
+
+
+
+
