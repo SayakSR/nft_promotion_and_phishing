c47fdc211a9a89706b1bef24701f5e4cb655c569
SayakSR <sayaksr05@gmail.com>
2022-06-05 12:01:00 -0700

    added logging

--- find_promoter_accounts/drivers/get_timeline_tweets.py
+++ find_promoter_accounts/drivers/get_timeline_tweets.py
@@ -2,6 +2,11 @@ import os
 import pandas as pd
 import re
 
+import logging
+
+logging.basicConfig(filename='pa.log', level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
 from psycopg2 import Timestamp
 
 # First party imports
@@ -10,6 +15,7 @@ from drivers.db_driver_tweets import *
 from drivers.date_time_stamp import *
 
 def fetch_timeline_tweets(userid,filepath):
+    logging.info(f"== Starting timeline processes for {userid} ===")
     os.system(f"twarc2 timeline --use-search --limit 100 {userid} {filepath}/{userid}.json")
     os.system(f"twarc2 csv {filepath}/{userid}.json {filepath}/{userid}.csv")
 
@@ -17,17 +23,20 @@ def process_timeline_tweets(userid,filepath):
     try:
         with open('seen_tweet_list.txt') as f: # Seen id list avoids storing data from user ids that have been seen before
             seen_tweet_list=[line.rstrip() for line in f]
+            logging.info("Initializing seen tweets file")
     except:
-        print("Need to initialize seen id file")
+        logging.info("Creating seen tweets file")
         seen_tweet_list=[]
+
     file=pd.read_csv(f'{filepath}/{userid}.csv')
+    logging.info("Read timeline for  seen tweets file")
 
     for index, row in file.iterrows():
 
          tweet_id=row['id']
 
          if tweet_id in seen_tweet_list:
-            print("Tweet already seen before")
+            logging.info(f"Tweet {tweet_id} already seen before, skipping")
             pass # ID has already been crawled before
 
          else:
@@ -41,8 +50,10 @@ def process_timeline_tweets(userid,filepath):
             replies=row['public_metrics.reply_count']
             user_id=row['author_id']
             user_name=row['author.username']
-
-            insert_data_into_table(2222,timestamp,tweet_id,user_id,user_name,tweet_text,likes,retweets,replies)
+            try: 
+                insert_data_into_table(2222,timestamp,tweet_id,user_id,user_name,tweet_text,likes,retweets,replies)
+            except:
+                logging.warning(f"DB JOB ID 2222: Error inserting entry into database - tweet:{tweet_id} for User:{user_id}")
 
     with open('seen_tweet_list.txt', 'a') as f:
         for item in seen_tweet_list:
