287a942b9df83421b26bb45eea7b98f72de2b8e5
SayakSR <sayaksr05@gmail.com>
2022-07-02 14:39:05 -0700

    tested 07022022

--- tracking_promoters/drivers/date_time_stamp.py
+++ tracking_promoters/drivers/date_time_stamp.py
@@ -24,3 +24,20 @@ def fetch_datestamp():
 
     return dstamp
 
+def convert_created_at(created_at): # Function to convert twitter created_at to python timestamp
+ 
+    import datetime
+    datetime_stamp=datetime.datetime.strptime(created_at,"%Y-%m-%dT%H:%M:%S.%fZ")
+    return datetime_stamp
+
+def convert_to_epoch(datetime_stamp): # Function to convert python timestamp to unix timestamp
+     import datetime
+     epoch=datetime.datetime.strptime(datetime_stamp,"%Y-%m-%dT%H:%M:%S.%fZ").timestamp()
+     return epoch
+
+def datetime_to_epoch(input): # Epoch timestamp
+
+    input=str(input)
+    import datetime
+    epoch=datetime.datetime.strptime(input,"%Y-%m-%d %H:%M:%S").timestamp()
+    return epoch
