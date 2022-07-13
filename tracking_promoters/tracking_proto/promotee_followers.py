c895071f11f85e20dd1248d44b050a70e3b99c07
SayakSR <sayaksr05@gmail.com>
2022-07-13 12:52:50 -0700

    proto

--- tracking_promoters/tracking_proto/promotee_followers.py
+++ tracking_promoters/tracking_proto/promotee_followers.py
@@ -48,7 +48,7 @@ def run_promotee_followers_main():
 
         scenario=0
         activate=0
-
+        print("Here")
         if int(timestamp)-int(promotee_time)>1800 and int(timestamp)-int(promotee_time)<3600: # 8 - 12 hrs
             scenario=1
             activate=1
